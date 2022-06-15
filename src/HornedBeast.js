4b0a1ee58742ef2f78c8faa29b217221987be05e
Rui Guo <guojiarui@gmail.com>
2022-06-14 20:03:25 -0700

    vote functionality added

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -2,10 +2,24 @@ import React from 'react';
 import './HornedBeast.css'
 
 class HornedBeast extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      votes:0
+    }
+  }
+  handleVotes = () => {
+    this.setState({
+      votes: this.state.votes + 1,
+      greeting: 'Hello!'
+    });
+  };
   render() {
     return (
       <article>
         <h3>{this.props.keyword}</h3>
+        <p>❤️ {this.state.votes}</p><br/>
+        <p onClick={this.handleVotes}>Click here to vote for me</p>
         <img
           src={this.props.image_url}
           alt={this.props.text}
