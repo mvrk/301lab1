d9fdf8d4d0fa1bbe01262f9a1cb626c5005d6753
Rui Guo <guojiarui@gmail.com>
2022-06-16 15:26:21 -0700

    modal function glitch fixed.

--- src/Main.js
+++ src/Main.js
@@ -4,27 +4,27 @@ import data from './data.json';
 import './Main.css';
 
 class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allBeasts: data
+
+    };
+  };
   render() {
-    let hBeast = [];
-    data.forEach((hornedBeast, index) => {
-      hBeast.push(
-        <HornedBeast
-          // _id={hornedBeast._id}
-          image_url={hornedBeast.image_url}
-          title={hornedBeast.title}
-          description={hornedBeast.description}
-          keyword={hornedBeast.keyword}
-          horns={hornedBeast.horns}
-          key={index}
-        />
-      )
-    })
     return (
       <main>
-        {hBeast}
-        <HornedBeast />
+        {
+          this.state.allBeasts.map((hornedBeast, index) => {
+            return <HornedBeast
+              hornedBeast={hornedBeast}//this is a whole beast obj = this.props
+              key={index}
+              openModal={this.props.handleOnShowModal}
+              handleOnHide={this.props.handleOnHide}
 
-
+            />
+          })
+        }
       </main>
     )
   }
